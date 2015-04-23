775f8e778e0d16190c4a14509fa23bfcbd8f03a6
michaelbiggers <mbiggers@knights.ucf.edu>
2015-04-23 01:25:01 -0400

    static images in place
    next step, calculate correct ratio for fullscreen and finish styling
    question pages

--- js/quiz.js
+++ js/quiz.js
@@ -3,6 +3,10 @@ var webCounter = 0;
 var graphicCounter = 0;
 var programCounter = 0;
 
+
+	var calcWidth = window.innerWidth;
+		var ratio = window.innerHeight/window.innerWidth;
+		var calcHeight = ratio*calcWidth;
 function myFunction() {
 
     var a = document.getElementById("question1").selectedIndex;
@@ -64,13 +68,128 @@ function myFunction() {
 
 /********* Logic returning results *********/
 	if (webCounter > graphicCounter && webCounter > programCounter){
-		alert("You're a web Designer!");
+		//alert("You're a web Designer!");
+	
+
+		var layer = new collie.Layer({
+		    width: calcWidth,
+		    height: calcHeight
+		});
+		 
+		collie.ImageManager.add({
+		    bubble: "img/bubble.png",
+		    background: "img/webDesign.png"
+		});
+
+		var background = new collie.DisplayObject({
+			width : layer.get("width"),
+			height : layer.get("height"),
+			fitImage : true,
+			backgroundImage : "background"
+			}).addTo(layer);
+		 
+		var bubble = new collie.DisplayObject({
+		    x: 55,
+		    y: 75,
+		    scaleX:.5,
+		    scaleY:.5,
+		   // mass: 1,
+		    backgroundImage: ""
+		}).addTo(layer);
+
+		collie.Timer.transition(bubble, 3000, {
+		        to : [55, 50],
+		        set : ["x", "y"],
+		        effect: collie.Effect.cubicEase
+		    });
+		 
+		collie.Renderer.addLayer(layer);
+		collie.Renderer.load(document.getElementById("container"));
+		collie.Renderer.start();
+		
 	}
 	else if (graphicCounter > webCounter && graphicCounter > programCounter){
-		alert("You're a graphic Designer!");
+		//alert("You're a graphic Designer!");
+	
+	
+		
+		
+
+		var layer = new collie.Layer({
+		    width: window.innerWidth,
+		    height: window.innerHeight
+		});
+		 
+		collie.ImageManager.add({
+		    bubble: "img/bubble.png",
+		    background: "img/graphicDesign.png"
+		});
+
+		var background = new collie.DisplayObject({
+			width : layer.get("width"),
+			height : layer.get("height"),
+			fitImage : true,
+			backgroundImage : "background"
+			}).addTo(layer);
+		 
+		var bubble = new collie.DisplayObject({
+		    x: 55,
+		    y: 75,
+		    scaleX:.5,
+		    scaleY:.5,
+		   // mass: 1,
+		    backgroundImage: "bubble"
+		}).addTo(layer);
+
+		collie.Timer.transition(bubble, 3000, {
+		        to : [55, 50],
+		        set : ["x", "y"],
+		        effect: collie.Effect.cubicEase
+		    });
+		 
+		collie.Renderer.addLayer(layer);
+		collie.Renderer.load(document.getElementById("container"));
+		collie.Renderer.start();
 	}
 	else if (programCounter > webCounter && programCounter > graphicCounter){
 		alert("You're a programmer!");
+
+		var layer = new collie.Layer({
+		    width: window.innerWidth,
+		    height: window.innerHeight
+		});
+		 
+		collie.ImageManager.add({
+		    bubble: "img/bubble.png",
+		    background: "img/webDevelop.png"
+		});
+
+		var background = new collie.DisplayObject({
+			width : layer.get("width"),
+			height : layer.get("height"),
+			fitImage : true,
+			backgroundImage : "background"
+			}).addTo(layer);
+		 
+		var bubble = new collie.DisplayObject({
+		    x: 55,
+		    y: 75,
+		    scaleX:.5,
+		    scaleY:.5,
+		   // mass: 1,
+		    backgroundImage: ""
+		}).addTo(layer);
+
+		collie.Timer.transition(bubble, 3000, {
+		        to : [55, 50],
+		        set : ["x", "y"],
+		        effect: collie.Effect.cubicEase
+		    });
+		 
+		collie.Renderer.addLayer(layer);
+		collie.Renderer.load(document.getElementById("container"));
+		collie.Renderer.start();
+	
 	}
 	else {
 		alert("You're a great Mix!");
